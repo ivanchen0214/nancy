f0179ba7d39d75a8afa8823349d46efdebff216e
chenjiahau <ivan.chen@dlinkcorp.com>
2020-08-07 10:22:39 +0800

    Update db settings.

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,14 @@
+//- firebase
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const serviceAccount = require("./permissions.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://learning-firebase-20200806.firebaseio.com"
+});
+
+//- express
 const express = require('express');
 const cors = require('cors');
 const app = express();
